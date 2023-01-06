5235a044ebb8031147e79b94157bb9c7537af5c6
Christopher Vidic <chrisvidic.cv@gmail.com>
2023-01-06 12:31:52 -0700

    feat: calculate reimbursement function

---
+++ technical_exercise.js
@@ -0,0 +1,27 @@
+
+function calculateReimbursement(projects, low_cost_cities, high_cost_cities){
+    const low_cost_travel_day = 45;
+    const high_cost_travel_day = 55;
+    const low_cost_full_day = 75
+    const high_cost_full_day = 85
+
+    let final_reimbursement = 0;
+
+    for(let project of projects){
+        let start_date = new Date(project["start_date"])
+        let end_date = new Date(project["end_date"])
+        let city = project["city"]
+
+        let full_days = end_date.getDate()-start_date.getDate()-1
+
+        if(low_cost_cities.has(city)){
+            final_reimbursement += (low_cost_travel_day*2)
+            final_reimbursement += (low_cost_full_day*full_days)
+        }else if(high_cost_cities.has(city)){
+            final_reimbursement += (high_cost_travel_day*2)
+            final_reimbursement += (high_cost_full_day*full_days)
+        }
+    }
+    return final_reimbursement
+}
+
