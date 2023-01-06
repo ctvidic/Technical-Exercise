d138de3bdffe251956dada53068543bf9fc3e3fd
Christopher Vidic <chrisvidic.cv@gmail.com>
2023-01-06 12:40:01 -0700

    doc: examples and additional documentation

--- technical_exercise.js
+++ technical_exercise.js
@@ -25,3 +25,15 @@ function calculateReimbursement(projects, low_cost_cities, high_cost_cities){
     return final_reimbursement
 }
 
+let low_cost_cities = new Set(["Pittsburgh", "Cleveland", "Detroit"])
+let high_cost_cities = new Set(["San Francisco", "New York", "Seattle"])
+let ex_projects = [{city: "New York", start_date: "2021-03-25", end_date: "2021-03-30"},
+{city: "Pittsburgh", start_date: "2021-03-12", end_date: "2021-03-15"}]
+
+let output = calculateReimbursement(ex_small_project, low_cost_cities,high_cost_cities)
+
+console.log(output)
+
+//O(N) time complexity algorithm that loops through all cities in a project and sums up their needed
+//reimbursement. Use a Set for storing cities for quick O(1) lookup, and store dates
+//as strings to be converted to Date Object. 
